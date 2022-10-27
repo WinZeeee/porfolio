75fa897965431446fc315ba23304d8f6cb7e09f5
Tran Hung Thinh <wintotv1998@gmail.com>
2022-10-27 12:05:38 +0700

    init

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        Hi, I am <span>Front end developer</span>
+      </h1>
+      <p>I am a frontend web developer. I can provide optimized clean code.</p>
+    </div>
+  );
+}
